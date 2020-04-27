a196db9f484427e0b7223835e077f92d86b5df24
Sviatoslav <swatoslaw112233@gmail.com>
2020-04-28 00:43:14 +0300

    Version_1.0

--- src/main/java/org/example/MyFileWriter.java
+++ src/main/java/org/example/MyFileWriter.java
@@ -7,12 +7,11 @@ import java.io.IOException;
 import java.util.List;
 
 public class MyFileWriter {
-    public boolean writeFile(List<String> list) throws IOException {
+    public void writeFile(List<String> list) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(new File("src/main/resources/output.txt")));
         for (int i = 1; i <= list.size(); i++) {
             writer.write(list.get(list.size() - i) + "\n");
         }
         writer.flush();
-        return true;
     }
 }
